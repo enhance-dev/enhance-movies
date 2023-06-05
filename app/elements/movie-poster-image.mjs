d8cda5dbc58029e949c0399c7379b77af2bdfe4c
Cole Peters <cole@colepeters.com>
2023-06-05 14:55:09 -0500

    Update movie poster image component layout

--- app/elements/movie-poster-image.mjs
+++ app/elements/movie-poster-image.mjs
@@ -1,27 +1,21 @@
 export default function Element ({ html, state }) {
   const { attrs, store } = state
-  const { height, width } = attrs
+  const { width } = attrs
   const { movie } = store
   const { poster_path } = movie
   return html`
       <style>
           .aspect-ratio-box {
-              padding-top: 150%;
+              aspect-ratio: 2 / 3;
           }
           img {
               filter: grayscale(0%);
           }
       </style>
-        <div class="aspect-ratio-box h-0 overflow-hidden relative">
-            <div class="absolute inset-bs0 inset-is0 si-100 sb-100">
-                <div class="relative">
-                    <img
-                        width="${width}" height="${height}"
-                        src="https://image.tmdb.org/t/p/w${width}${poster_path}"
-                        class="object-cover si-100 sb-100"
-                    />
-                </div>
-        </div>
+        <div class="aspect-ratio-box overflow-hidden">
+            <img
+                src="https://image.tmdb.org/t/p/w${width}${poster_path}"
+                class="object-cover"
+            />
         </div>`
 }
-
