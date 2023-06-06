64d3b1629ccad151683b1b2339a85baf9e891cb4
macdonst <simon.macdonald@gmail.com>
2023-06-06 14:50:13 -0400

    Add meta description
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/head.mjs
+++ app/head.mjs
@@ -9,6 +9,7 @@ export default function Head () {
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="Enhance Movie is a non-trivial demo application built on top of the TMDB (The Movie Database) API">
       <title>Enhance Movie</title>
       ${linkTag()}
       <link rel="icon" href="/_public/favicon.svg">
