3bd9471a9cd5135142c6095f8f1c2c5bb8aa7861
macdonst <simon.macdonald@gmail.com>
2023-06-07 14:26:55 -0400

    Fix imdb link for person page
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/elements/movie/person/links.mjs
+++ app/elements/movie/person/links.mjs
@@ -13,10 +13,10 @@ export default function Element ({ html, state }) {
       }
     </style>
     <div class="flex mie-auto">
-        <movie-button href="${homepage}" label="Website" remote>
+        ${homepage && `<movie-button href="${homepage}" label="Website" remote>
             ${link}
-        </movie-button>
-        <movie-button href="https://www.imdb.com/title/${imdb_id}/" label="IMDB" remote>
+        </movie-button>`}
+        <movie-button href="https://www.imdb.com/name/${imdb_id}/" label="IMDB" remote>
             ${imdb}
         </movie-button>
     </div>
