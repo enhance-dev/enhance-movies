0c8520562ec327b89397458c38562363a5a6d863
macdonst <simon.macdonald@gmail.com>
2023-06-09 14:10:12 -0400

    Space out the sidebar
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/elements/movie-sidebar.mjs
+++ app/elements/movie-sidebar.mjs
@@ -10,15 +10,15 @@ export default function MovieSidebar ({ html, state }) {
     </style>
     <div class="p0 flex">
         <nav>
-            <h2>Discover</h2>
+            <h2 class="font-bold">Discover</h2>
             <ul class="list-none">
-                <li><a href="/?category=popular&page=1">Popular</a></li>
-                <li><a href="/?category=top_rated&page=1">Top Rated</a></li>
-                <li><a href="/?category=upcoming&page=1">Upcoming</a></li>
+                <li class="pb-5 pi-1"><a href="/?category=popular&page=1">Popular</a></li>
+                <li class="pb-5 pi-1"><a href="/?category=top_rated&page=1">Top Rated</a></li>
+                <li class="pb-5 pi-1"><a href="/?category=upcoming&page=1">Upcoming</a></li>
             </ul>
-            <h2>Genres</h2>
+            <h2 class="font-bold">Genres</h2>
             <ul class="list-none">
-                ${genres.map(genre => `<li><a href="genre?id=${genre.id}&name=${genre.name}&page=1">${genre.name}</a></li>`).join('')}
+                ${genres.map(genre => `<li class="pb-5 pi-1"><a href="genre?id=${genre.id}&name=${genre.name}&page=1">${genre.name}</a></li>`).join('')}
             </ul>
         </nav>
     </div>
