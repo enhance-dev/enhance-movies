5c30d07a036c9d3c86c7f7214cdd971c24ca6368
macdonst <simon.macdonald@gmail.com>
2023-06-09 10:45:36 -0400

    Refactor API calls

--- app/tmdbAPI/index.mjs
+++ app/tmdbAPI/index.mjs
@@ -1,7 +1,6 @@
 import tiny from 'tiny-json-http'
 
 const {
-  TMDB_API_KEY,
   TMDB_API_BASE_URL,
   TMDB_API_VERSION,
   TMDB_API_TOKEN
@@ -9,61 +8,56 @@ const {
 
 const baseUrl = `${TMDB_API_BASE_URL}/${TMDB_API_VERSION}`
 
-const getMovies = async function(type, page) {
-  const response = await tiny.get({url: `${baseUrl}/movie/${type}?page=${page}&api_key=${TMDB_API_KEY}`})
+const getJSON = async function(url) {
+  const response = await tiny.get({
+    url: `${baseUrl}${url}`,
+    headers: {
+      'Authorization': `Bearer ${TMDB_API_TOKEN}`,
+      'accept': 'application/json'
+    }
+  })
   return response.body
+}
+
+const getMovies = async function(type, page) {
+  return getJSON(`/movie/${type}?page=${page}`)
 }
 
 const getMovie = async function(id) {
-  const response = await tiny.get({url: `${baseUrl}/movie/${id}?api_key=${TMDB_API_KEY}&lang=en`})
-  return response.body
+  return getJSON(`/movie/${id}?lang=en-US`)
 }
 
 const getMovieWithCast = async function(id, page = '1', sort_by = 'popularity.desc') {
-  const response = await tiny.get({url: `${baseUrl}/discover/movie?with_cast=${id}&page=${page}&sort_by=${sort_by}&api_key=${TMDB_API_KEY}`})
-  return response.body
+  return getJSON(`/discover/movie?with_cast=${id}&page=${page}&sort_by=${sort_by}`)
 }
 
 const getCredits = async function(id) {
-  const response = await tiny.get({url: `${baseUrl}/movie/${id}/credits?api_key=${TMDB_API_KEY}`})
-  return response.body
+  return getJSON(`/movie/${id}/credits`)
 }
 
 const getVideos = async function(id) {
-  const response = await tiny.get({url: `${baseUrl}/movie/${id}/videos?api_key=${TMDB_API_KEY}`})
-  return response.body
+  return getJSON(`/movie/${id}/videos`)
 }
 
 const getRecommendations = async function(id, page) {
-  const response = await tiny.get({url: `${baseUrl}/movie/${id}/recommendations?page=${page}&api_key=${TMDB_API_KEY}`})
-  return response.body
+  return getJSON(`/movie/${id}/recommendations?page=${page}`)
 }
 
 const getPerson = async function(id) {
-  const response = await tiny.get({url: `${baseUrl}/person/${id}?api_key=${TMDB_API_KEY}`})
-  return response.body
+  return getJSON(`/person/${id}`)
 }
 
 const searchMovies = async function(q, page) {
-  const response = await tiny.get({url: `${baseUrl}/search/movie?query=${q}&page=${page}&api_key=${TMDB_API_KEY}`})
-  return response.body
+  return getJSON(`/search/movie?query=${q}&page=${page}`)
 }
 
 const getGenres = async function() {
-  const response = await tiny.get({url: `${baseUrl}/genre/movie/list?api_key=${TMDB_API_KEY}`})
-  return response.body.genres
+  const response = await getJSON(`/genre/movie/list`)
+  return response.genres
 }
 
 const getGenreMovies = async function(id, page, sort_by) {
-  const response = await tiny.get(
-    {
-      url: `${baseUrl}/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=${sort_by}&with_genres=${id}`,
-      headers: {
-        'Authorization': `Bearer ${TMDB_API_TOKEN}`,
-        'accept': 'application/json'
-      }
-    })
-  return response.body
+  return getJSON(`/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=${sort_by}&with_genres=${id}`)
 }
 
 export { getCredits, getMovie, getMovies, getVideos, getRecommendations, getPerson, getMovieWithCast, searchMovies, getGenres, getGenreMovies }
