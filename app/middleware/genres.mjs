a12bf63d418ac7b0c6fbe8b089248e07f09dfd18
macdonst <simon.macdonald@gmail.com>
2023-06-16 18:28:59 -0400

    Cache genres
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/middleware/genres.mjs
+++ app/middleware/genres.mjs
@@ -1,6 +1,21 @@
 import { getGenres } from '../tmdbAPI/index.mjs'
+import data from '@begin/data'
 
 export async function fetchGenres (req) {
-  const genres = await getGenres()
+  console.time('genres')
+  let results = await data.get({
+    table: 'data',
+    key: 'genres',
+  })
+  let genres = results?.genres
+  if (!genres) {
+    genres = await getGenres()
+    await data.set({
+      table: 'data',
+      key: 'genres',
+      genres
+    })
+  }
   req.genres = genres
+  console.timeEnd('genres')
 }
