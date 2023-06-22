ed58c07c7cb7416f1be06b8e676875f333650a92
Cole Peters <cole@colepeters.com>
2023-06-22 12:34:12 -0500

    Update language display

--- app/elements/movie/summary/details.mjs
+++ app/elements/movie/summary/details.mjs
@@ -1,16 +1,17 @@
+import languages from '../../../lib/languages.mjs'
+
 export default function MovieSummaryDetails ({ html, state }) {
   const { store } = state
   const { movie } = store
-  const { release_date, runtime, spoken_languages } = movie
+  const { release_date, runtime, original_language } = movie
   const year = (new Date(release_date)).getFullYear()
-  const lang = spoken_languages[0].name
+  const language = languages.find(lang => lang.iso_639_1 === original_language).english_name
   return html`
     <style>
     :host p {
         color: var(--palette-warning-main);
     }
     </style>
-    <p class="text-1 font-bold leading-none uppercase">${lang}/${runtime} MIN./${year}</p>
+    <p class="text-1 font-bold leading-none uppercase">${language}/${runtime} min./${year}</p>
   `
 }
-
