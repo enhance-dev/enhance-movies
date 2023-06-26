e8ff7e5869067a45fa8442e8ad73abcebdc75ae8
Cole Peters <cole@colepeters.com>
2023-06-26 14:22:30 -0500

    Add movie background fadeout

--- app/elements/movie/summary/wrapper.mjs
+++ app/elements/movie/summary/wrapper.mjs
@@ -3,36 +3,41 @@ export default function MovieSummaryWrapper ({ html, state }) {
   const { movie = {} } = store
   const { backdrop_path = '' } = movie
 
+  const backdropFilter = `${backdrop_path ? 'blur(2px) brightness(80%)' : 'none' }`
+
   return html`
     <style>
     :host {
       display: block;
+      position: relative;
       background-image: url("https://image.tmdb.org/t/p/w1280/${backdrop_path}");
       background-size: cover;
     }
 
     section {
-      backdrop-filter: ${backdrop_path ? 'blur(2px) brightness(30%)' : 'none' };
-      -webkit-backdrop-filter: ${backdrop_path ? 'blur(2px) brightness(30%)' : 'none' };
+      margin-top: calc(var(--space-0) * -1);
+      display: block;
+      max-width: 64rem;
     }
 
-    section > div {
-        display: block;
-        max-width: 60rem;
+    .fadeout {
+      inset: 0;
+      background-image: linear-gradient(to bottom, transparent, var(--dark)), linear-gradient(to right, var(--dark), transparent);
+      backdrop-filter: ${backdropFilter};
+      -webkit-backdrop-filter: ${backdropFilter};
     }
 
     @media only screen and (min-width: 48em) {
-      section > div {
+      section {
         display: grid;
         grid-template-columns: 40% 60%;
         margin-bottom: 2.5rem;
       }
     }
     </style>
-    <section class="pb4 mbe0">
-      <div class="mi-auto">
-        <slot></slot>
-      </div>
+    <div class="fadeout absolute"></div>
+    <section class="pb4 mbe0 relative z1">
+      <slot></slot>
     </section>
   `
 }
