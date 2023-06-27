50449e2a16da2e1e0bb10be3ccfe019c82f5e413
Cole Peters <cole@colepeters.com>
2023-06-27 11:52:56 -0500

    Swap meta for root style

--- app/head.mjs
+++ app/head.mjs
@@ -10,13 +10,16 @@ export default function Head () {
         <meta charset="utf-8" />
         <meta name="view-transition" content="same-origin" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="color-scheme" content="dark light">
         <meta name="description" content="Enhance Movie is a non-trivial demo application built on top of the TMDB (The Movie Database) API" />
         <title>Enhance Movie</title>
         <link rel="stylesheet" href="/_public/keyframes.css" />
         ${linkTag()}
         <link rel="icon" href="/_public/favicon.svg" />
         <style>
+          :root {
+            color-scheme: dark;
+          }
+
           body {
             background: var(--dark);
             color: var(--light);
