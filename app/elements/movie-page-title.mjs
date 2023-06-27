c93eaa2432773b627988f8c8e11ebd25ce8fe7e8
Cole Peters <cole@colepeters.com>
2023-06-27 12:36:46 -0500

    Don't render related movies if there aren't any

--- app/elements/movie-page-title.mjs
+++ app/elements/movie-page-title.mjs
@@ -1,9 +1,13 @@
 export default function MoviePageTitle ({ html, state }) {
   const { store } = state
-  const { title = {} } = store
+  const { title = {}, shows } = store
   const { primary, secondary } = title
 
+  const content = shows.length
+    ? `<h1 class="text2 font-medium">${primary} ${secondary}</h1>`
+    : ``
+
   return html`
-  <h1 class="text2 font-medium">${primary} ${secondary}</h1>
-`
+    ${content}
+  `
 }
