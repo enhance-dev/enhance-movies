72566300ea810b9e498f1a79fbc0ec20afa9f84d
Cole Peters <cole@colepeters.com>
2023-06-27 12:32:10 -0500

    Fix genre icon layout

--- app/elements/movie/summary/genres.mjs
+++ app/elements/movie/summary/genres.mjs
@@ -11,7 +11,7 @@ export default function MovieSummaryGenres ({ html, state }) {
     </style>
     <typography-h3>Genres</typography-h3>
     <p class="flex flex-wrap align-items-center">
-        ${genres.map(genre => `<a class="flex align-items-center mie0 text-2 tracking2 font-bold uppercase" href="/genre?id=${id}&name=${genre.name}&page=1"><svg height="24" width="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+        ${genres.map(genre => `<a class="flex align-items-center mie0 text-2 tracking2 font-bold uppercase" href="/genre?id=${id}&name=${genre.name}&page=1"><svg height="16" width="16" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mie-6">
             <use xlink:href="#svg-play">
           </svg>${genre.name}</a>`).join('')}
     </p>
