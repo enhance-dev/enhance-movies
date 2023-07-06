57ffa33ac085ffbb7eba9c9cf02ca5e5f0dfbb23
Cole Peters <cole@colepeters.com>
2023-07-06 15:30:17 -0500

    Update descriptions

--- app/head.mjs
+++ app/head.mjs
@@ -11,13 +11,13 @@ export default function Head () {
         <meta name="view-transition" content="same-origin" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="title" content="Enhance Movies" />
-        <meta name="description" content="Enhance Movie is a non-trivial demo application built on top of the TMDB (The Movie Database) API." />
+        <meta name="description" content="A demo application built with Enhance, the HTML first framework. Powered by Begin and The Movies DB." />
         <meta name="theme-color" content="#a0094a"/>
         <!-- Open Graph -->
         <meta property='og:type' content='website' />
         <meta property='og:url' content='https://local-fih.begin.app/' />
         <meta property='og:title' content='Enhance Movies' />
-        <meta property='og:description' content='Enhance Movie is a non-trivial demo application built on top of the TMDB (The Movie Database) API.' />
+        <meta property='og:description' content='A demo application built with Enhance, the HTML first framework. Powered by Begin and The Movies DB.' />
         <!-- meta property='og:image' content='https://local-fih.begin.app/enhance-meta-image.jpg' />
         <meta property='og:image:width' content='1200' />
         <meta property='og:image:height' content='628' / -->
@@ -25,7 +25,7 @@ export default function Head () {
         <meta property='twitter:card' content='summary_large_image' />
         <meta property='twitter:url' content='https://local-fih.begin.app/' />
         <meta property='twitter:title' content='Enhance Movies' />
-        <meta property='twitter:description' content='Enhance Movie is a non-trivial demo application built on top of the TMDB (The Movie Database) API.' />
+        <meta property='twitter:description' content='A demo application built with Enhance, the HTML first framework. Powered by Begin and The Movies DB.' />
         <!-- meta property='twitter:image' content='https://local-fih.begin.app/enhance-meta-image.jpg' /-->
         <title>Enhance Movies</title>
         <link rel="stylesheet" href="/_public/keyframes.css" />
