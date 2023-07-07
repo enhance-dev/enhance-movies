dd69796cb8eb2b8c51c01254c670a88cd017c02e
Cole Peters <cole@colepeters.com>
2023-07-07 16:42:57 -0500

    Fix movie genre link

--- app/elements/movie/summary/genres.mjs
+++ app/elements/movie/summary/genres.mjs
@@ -1,7 +1,7 @@
 export default function MovieSummaryGenres ({ html, state }) {
   const { store } = state
   const { movie } = store
-  const { id, genres = [] } = movie
+  const { genres = [] } = movie
   return html`
     <style>
       :host {
@@ -11,7 +11,7 @@ export default function MovieSummaryGenres ({ html, state }) {
     </style>
     <h2 class='text-1 font-bold mb-2 uppercase tracking1'>Genres</h2>
     <p class="flex flex-wrap align-items-center">
-        ${genres.map(genre => `<a class="flex align-items-center mie0 text-2 tracking2 font-bold uppercase" href="/genre?id=${id}&name=${genre.name}&page=1"><svg height="16" width="16" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mie-6">
+        ${genres.map(genre => `<a class="flex align-items-center mie0 text-2 tracking2 font-bold uppercase" href="/genre?id=${genre.id}&name=${genre.name}&page=1"><svg height="16" width="16" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mie-6">
             <use xlink:href="#svg-play">
           </svg>${genre.name}</a>`).join('')}
     </p>
