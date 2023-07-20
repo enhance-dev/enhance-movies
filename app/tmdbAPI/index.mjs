49fdcd282798f90e70968442de63b0ec1e9a11d9
macdonst <simon.macdonald@gmail.com>
2023-07-20 16:12:26 -0400

    Mock when no API key
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/tmdbAPI/index.mjs
+++ app/tmdbAPI/index.mjs
@@ -1,4 +1,5 @@
 import tiny from 'tiny-json-http'
+import { getMockCredits, getMockMovie, getMockMovies, getMockVideos, getMockRecommendations, getMockPerson, getMockMovieWithCast, searchMockMovies, getMockGenres, getMockGenreMovies } from './mock.mjs'
 
 const TMDB_API_BASE_URL = 'https://api.themoviedb.org'
 const TMDB_API_VERSION = '3'
@@ -20,44 +21,84 @@ const getJSON = async function(url) {
 }
 
 const getMovies = async function(type, page) {
-  return getJSON(`/movie/${type}?page=${page}`)
+  if (TMDB_API_TOKEN) {
+    return getJSON(`/movie/${type}?page=${page}`)
+  } else {
+    return await getMockMovies()
+  }
 }
 
 const getMovie = async function(id) {
-  return getJSON(`/movie/${id}?lang=en-US`)
+  if (TMDB_API_TOKEN) {
+    return getJSON(`/movie/${id}?lang=en-US`)
+  } else {
+    return await getMockMovie()
+  }
 }
 
 const getMovieWithCast = async function(id, page = '1', sort_by = 'popularity.desc') {
-  return getJSON(`/discover/movie?with_cast=${id}&page=${page}&sort_by=${sort_by}`)
+  if (TMDB_API_TOKEN) {
+    return getJSON(`/discover/movie?with_cast=${id}&page=${page}&sort_by=${sort_by}`)
+  } else {
+    return await getMockMovieWithCast()
+  }
 }
 
 const getCredits = async function(id) {
-  return getJSON(`/movie/${id}/credits`)
+  if (TMDB_API_TOKEN) {
+    return getJSON(`/movie/${id}/credits`)
+  } else {
+    return await getMockCredits()
+  }
 }
 
 const getVideos = async function(id) {
-  return getJSON(`/movie/${id}/videos`)
+  if (TMDB_API_TOKEN) {
+    return getJSON(`/movie/${id}/videos`)
+  } else {
+    return await getMockVideos()
+  }
 }
 
 const getRecommendations = async function(id, page) {
-  return getJSON(`/movie/${id}/recommendations?page=${page}`)
+  if (TMDB_API_TOKEN) {
+    return getJSON(`/movie/${id}/recommendations?page=${page}`)
+  } else {
+    return await getMockRecommendations()
+  }
 }
 
 const getPerson = async function(id) {
-  return getJSON(`/person/${id}`)
+  if (TMDB_API_TOKEN) {
+    return getJSON(`/person/${id}`)
+  } else {
+    return await getMockPerson()
+  }
 }
 
 const searchMovies = async function(q, page) {
-  return getJSON(`/search/movie?query=${q}&page=${page}`)
+  if (TMDB_API_TOKEN) {
+    return getJSON(`/search/movie?query=${q}&page=${page}`)
+  } else {
+    return await searchMockMovies()
+  }
 }
 
 const getGenres = async function() {
-  const response = await getJSON(`/genre/movie/list`)
-  return response.genres
+  if (TMDB_API_TOKEN) {
+    const response = await getJSON(`/genre/movie/list`)
+    return response.genres
+  } else {
+    return await getMockGenres()
+  }
 }
 
 const getGenreMovies = async function(id, page, sort_by) {
-  return getJSON(`/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=${sort_by}&with_genres=${id}`)
+  if (TMDB_API_TOKEN) {
+    return getJSON(`/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=${sort_by}&with_genres=${id}`)
+  } else {
+    return await getMockGenreMovies()
+  }
 }
 
 export { getCredits, getMovie, getMovies, getVideos, getRecommendations, getPerson, getMovieWithCast, searchMovies, getGenres, getGenreMovies }
