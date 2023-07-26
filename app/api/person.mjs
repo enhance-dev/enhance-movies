fa407a146496176b4bbbbc080f0e6cf1fb586381
Cole Peters <cole@colepeters.com>
2023-07-26 14:30:32 -0500

    Add baseUrl for api/person, fix typo

--- app/api/person.mjs
+++ app/api/person.mjs
@@ -7,10 +7,11 @@ export async function fetchPerson (req) {
   const { id, page = 1 } = req.query
   const person = await getPerson(id, page)
   const shows = await getMovieWithCast(id, page)
+  const baseUrl = `/person?id=${id}`
   return {
     json: { title: {
       primary: "Also appears in",
       secondary: ""
-    }, person, genres: req.genres, shows }
+    }, person, genres: req.genres, shows, baseUrl }
   }
 }
