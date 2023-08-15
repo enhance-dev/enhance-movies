a15ca1e8e92c0018df2b3f9555779c4a7b2680d1
Cole Peters <cole@colepeters.com>
2023-08-15 15:26:45 -0700

    Remove view transition meta

--- app/head.mjs
+++ app/head.mjs
@@ -8,7 +8,6 @@ export default function Head () {
     <html lang="en">
       <head>
         <meta charset="utf-8" />
-        <meta name="view-transition" content="same-origin" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="title" content="Enhance Movies" />
         <meta name="description" content="A demo application built with Enhance, the HTML first framework. Powered by Begin and The Movies DB." />
